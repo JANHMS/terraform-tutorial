6b8fadafe0f842841b1eaa1f1628b274c1afade6
JANHMS <jan@kooperativ.net>
2022-11-02 16:00:15 +0100

    feat: terraform tutorial

---
+++ learn-terraform-aws-instance/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-08d70e59c07c61a3a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
